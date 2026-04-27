898e3899cff323d2e42c9c82b0a35ded9e758207
oshanYelena <oshanranasinghe25@gmail.com>
2026-04-27 12:43:34 +0530

    ai-chatbot: add chat summarizer

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from openai import OpenAI
 
 from app.core.config import settings
@@ -7,7 +9,7 @@ class LLMService:
         self.client = OpenAI(api_key=settings.OPENAI_API_KEY)
         self.model = settings.OPENAI_MODEL
 
-    def generate_reply(self, message:str) -> str:
+    def generate_reply(self, message: List[dict]) -> str:
         response = self.client.chat.completions.create(
             model=self.model,
             messages=[{
@@ -18,5 +20,22 @@ class LLMService:
 
         )
         return response.choices[0].message.content
+
+    def summarize_messages(self, messages: List[dict])-> str:
+        responses = self.client.chat.completions.create(
+            model=self.model,
+            messages = [{
+                "role": "system",
+                "content": """
+                Summarize this conversation for future context. 
+                Preserve user preferences, goals, important facts, decisions, 
+                and unresolved questions. Keep it concise."""
+            },{
+                "role": "user",
+                "content": str(messages)
+            }]
+        )
+
+        return responses.choices[0].message.content
 
 llm_service = LLMService()
