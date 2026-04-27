feada6a37a302a79d0068200969b9ff325f6d055
oshanYelena <oshanranasinghe25@gmail.com>
2026-04-27 11:03:56 +0530

    ai-chatbot: env constant name changed

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     APP_Name: str = "Simple AI Chatbot"
-    APP_Version:str = "0.1.0"
+    APP_VERSION:str = "0.1.0"
     ENVIRONMENT:str = "development"
 
     class Config:
