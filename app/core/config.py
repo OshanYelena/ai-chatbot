ae0a6cbb5a0d91b339bf7bf8150b1cc2e9e8dc95
oshanYelena <oshanranasinghe25@gmail.com>
2026-04-27 11:02:27 +0530

    ai-chatbot: env configurations added

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    APP_Name: str = "Simple AI Chatbot"
+    APP_Version:str = "0.1.0"
+    ENVIRONMENT:str = "development"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
